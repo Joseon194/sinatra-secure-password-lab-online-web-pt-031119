62978b6f531bbd1534a9cfc164200e5720016719
Dennis Chung <chungdennis6@gmail.com>
2019-07-09 16:01:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 require "./config/environment"
 require "./app/models/user"
-require 'pry'class ApplicationController < Sinatra::Base
+require 'pry'
+class ApplicationController < Sinatra::Base
 
   configure do
     set :views, "app/views"
